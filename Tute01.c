c305330b5f85daa07fc4078713446f5686e99ab6
chandeep 99 <87350613+it21023996@users.noreply.github.com>
2021-07-13 03:47:24 +0000

    code

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,13 @@
 #include <stdio.h>
 
 int main() {
-  
+  float num1,num2,avg=0;
+  printf("enter num 1: ");
+  scanf("%f",&num1);
+  printf("enter num 2: ");
+  scanf("%f",&num2);
+  avg = (num1 + num2)/2;
+  printf("avg = %.2f",avg);
   return 0;
 }
 
